f03307c9d56ee3c484dfb4c955647145234c43ed
AlisonFDLHC <104040953+AlisonFDLHC@users.noreply.github.com>
2022-07-15 14:16:13 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale-1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    </head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <body>
+        <footer class="container-fluid bg-dark d-flex flex-column align-items-center justify-content-center">
+            <a href="" class="text-white display-6 text-decoration-none my-4"><img src="logo.png" alt=""></a>
+            <div class="container-sm d-flex align-items-center justify-content-center gap-5 footerLinks">
+                
+                    <address>
+                        <a href="mailto:lls@llsinformatica.com.br" class="text-white display-6 fs-5 text-decoration-none">lls@llsinformatica.com.br</a>
+                    </address>
+                            
+                    <address class="text-center">
+                        <a href="https://www.google.com/maps/place/LLS+INFORM%C3%81TICA/@-23.0770683,-47.2006232,21z/data=!4m5!3m4!1s0x94c8b4bbebe3820b:0xfcc1b8bbadf46a57!8m2!3d-23.0770705!4d-47.2006013" class="text-white display-6 fs-5 text-decoration-none">Avenida Conceição, 1898</br>Cidade Nova II - Indaiatuba/SP</a>
+                    </address>
+                        
+                    <address class="text-center">
+                        <a href="tel:+551938758955" class="text-white display-6 fs-5 text-decoration-none">Telefone: (19) 3875 - 8955</br><a href="tel:+5519997241470"class="text-white display-6 fs-5 text-decoration-none">(19) 9 9724 - 1470</a></a>
+                    </address>
+            
+            </div>
+            <div class="d-flex align-items-center justify-content-center gap-4">
+                <img src="mspartner.jpg" alt="" style="width:12rem;">
+                <a href="https://www.facebook.com/llsinformatica.com.br?_sm_nck=1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                    </svg>
+                </a>
+            </div>
+            <small class="display-6 fs-5 text-center text-white my-4">LLS Suporte e Comércio de Informática LDTA - © 2018.</small>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    </body>
+</html>
